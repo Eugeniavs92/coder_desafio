27e49b7940a302d8cf61f670702b17030fec8a9f
Eugenia Vargas Silva <eugenia.vargassilva@gmail.com>
2020-10-11 23:22:04 -0300

    reestructuracion de pagina

---
+++ js/tienda.js
@@ -0,0 +1,25 @@
+'use strict'
+
+const cartas = [{
+        nombe: 'CARTA NATAL COMPLETA',
+        precio: '2200',
+        imagen: '../assets/imagenes/imgcartanatal.jpg'
+    },
+    {
+        nombe: 'REVOLUCIÓN SOLAR',
+        precio: '1200',
+        imagen: '../assets/imagenes/imgcartanatal.jpg'
+    }
+]
+
+const cuadros = [{
+        nombe: 'MAPA ESTELAR PERSONALIZADO 40x30cm',
+        precio: '1800',
+        imagen: '../assets/imagenes/imgcartanatal.jpg'
+    },
+    {
+        nombe: 'MAPA ESTELAR PERSONALIZADO 30x20cm',
+        precio: '1300',
+        imagen: '../assets/imagenes/imgcartanatal.jpg'
+    }
+]
